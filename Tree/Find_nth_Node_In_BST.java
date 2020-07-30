27f88e79bb69e5f3816c211dad687e7abf38b1a3
Mri07 <mrinal7331@gmail.com>
2020-07-30 22:15:01 +0530

    FIND NODE

--- Tree/Find_nth_Node_In_BST.java
+++ Tree/Find_nth_Node_In_BST.java
@@ -1,55 +1,85 @@
 package algorithms.Tree;
 
-public class Find_nth_Node_In_BST 
-{
+public class Find_nth_Node_In_BST {
 	static Node root = null;
-	static int count=0;
-	public static void main(String[] args) 
-	{
+	static int count = 0;
+
+	public static void main(String[] args) {
 		createTree();
 		show(root);
 		System.out.println();
-		
-		findNthNode(3);		
+
+		findNthNodeInorder(3);
+	}
+
+	private static void findNthNodeInorder(int i) {
+		preorder(root, i);
+		count = 0;
+		inorder(root, i);		
+		count = 0;
+		postorder(root, i);
+	}
+
+	private static void preorder(Node root, int n) {
+		if (root == null)
+			return;
+
+		count++;
+		if (count == n)
+			System.out.println("PRE-ORDER => "+root.data);
+		preorder(root.left, n);
+
+		preorder(root.right, n);
 	}
 	
-	private static void findNthNode(int i) 
-	{		
-	   inorder(root,i);				
+	private static void inorder(Node root, int n) {
+		if (root == null)
+			return;
+
+		if (count <= n) {
+			inorder(root.left, n);
+			count++;
+
+			if (count == n)
+				System.out.println("IN-ORDER => "+root.data);
+
+			inorder(root.right, n);
+		}
 	}
 
-	private static void inorder(Node root,int n) 
-	{			
-		if( root == null )return;		
+	private static void postorder(Node root, int n) {
+		if (root == null)
+			return;
 		
-		inorder( root.left,n);
-		count++;
-			
-		if(count == n )System.out.println(root.data);
+		postorder(root.left, n);
 
-		inorder( root.right,n);			
+		postorder(root.right, n);
+		
+		count++;
+		if (count == n)
+			System.out.println("POST-ORDER => "+root.data);
+
 	}
 
-	private static void createTree() 
-	{
+	private static void createTree() {
 		root = new Node(1);
 		root.left = new Node(2);
 		root.right = new Node(3);
-				
+
 		root.right.left = new Node(6);
 		root.right.right = new Node(7);
-		
+
 		root.left.left = new Node(4);
-		root.left.right = new Node(5);		
+		root.left.right = new Node(5);
 	}
-	
-	private static void show(Node ptr) 
-	{
-		if(ptr == null) return;
-		
+
+	private static void show(Node ptr) {
+		if (ptr == null)
+			return;
+
 		show(ptr.left);
-		System.out.print(ptr.data+" ");
-		show(ptr.right);		
+		System.out.print(ptr.data + " ");
+		show(ptr.right);
 	}
 
 }
